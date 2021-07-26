5518169418987546c768fbb99afb0ba50bb49d93
clarice_r <claricerdallorto@gmail.com>
2021-07-26 20:06:32 +0100

    Started on fade animations for the mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,12 @@
 const menuBtn = document.querySelector('#menu-btn');
+const mobileMenu = document.querySelector('#mobile-menu');
 
 menuBtn.addEventListener('click', () => {
     if(menuBtn.classList.contains('menu-toggle')) {
         menuBtn.classList.remove('menu-toggle');
+        mobileMenu.classList.add('hide');
     } else {
         menuBtn.classList.add('menu-toggle');
+        mobileMenu.classList.remove('hide')
     }
 })
