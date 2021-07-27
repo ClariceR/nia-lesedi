25c12a45fd7e2c2d973886f2da0c6a4e956b2bca
clarice_r <claricerdallorto@gmail.com>
2021-07-27 11:47:34 +0100

    Created fade for mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -4,9 +4,18 @@ const mobileMenu = document.querySelector('#mobile-menu');
 menuBtn.addEventListener('click', () => {
     if(menuBtn.classList.contains('menu-toggle')) {
         menuBtn.classList.remove('menu-toggle');
-        mobileMenu.classList.add('hide');
+        mobileMenu.classList.remove('anim-fade-in');
+        // mobileMenu.classList.add('hide');
+        mobileMenu.classList.add('anim-fade-out')
     } else {
         menuBtn.classList.add('menu-toggle');
-        mobileMenu.classList.remove('hide')
+        mobileMenu.classList.remove('hide');
+        mobileMenu.classList.remove('anim-fade-out');
+        mobileMenu.classList.add('anim-fade-in');
     }
 })
+
+// menuBtn.addEventListener('click', () => {
+//     menuBtn.classList.toggle("menu-toggle");
+//     mobileMenu.classList.toggle("fadeIn");
+// })
